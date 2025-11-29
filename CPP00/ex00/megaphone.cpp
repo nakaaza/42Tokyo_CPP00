251133cc2efbe0244dae71acad95586278392701
Taichi Nakaza <tnakaza@c1r7s8.42tokyo.jp>
2025-11-29 14:10:35 +0900

    Fix CPP00 ex00

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -5,13 +5,16 @@ int	main(int argc, char **argv)
 	int	i = 0;
 	int	j;
 
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
 	while (++i < argc)
 	{
 		j = -1;
 		while (argv[i][++j] != '\0')
 			std::cout << static_cast<char>(std::toupper(argv[i][j]));
-		if (i + 1 < argc)
-			std::cout << " ";
 	}
 	std::cout << std::endl;
 	return (0);
