c5075387dc976917197c1745db82b1307c908995
Taichi Nakaza <tnakaza@c1r7s8.42tokyo.jp>
2025-11-29 15:25:13 +0900

    Fix CPP01 ex05

--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -5,6 +5,11 @@ int	main(int argc, char **argv)
 {
 	Harl	harl;
 
+	if (argc < 2)
+	{
+		std::cerr << "Invalid argument: ./ex05 <level> (<level> ...)" << std::endl;
+		return (1);
+	}
 	int i = 0;
 	while (++i < argc)
 		harl.complain(argv[i]);
